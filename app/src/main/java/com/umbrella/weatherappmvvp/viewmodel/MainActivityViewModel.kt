a42ad1bb682cd3f0beefa9634552d414b4da3382
Weret90 <apostol-ea@mail.ru>
2021-07-19 00:15:42 +0900

    Third commit

--- app/src/main/java/com/umbrella/weatherappmvvp/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/umbrella/weatherappmvvp/viewmodel/MainActivityViewModel.kt
@@ -6,8 +6,7 @@ import androidx.lifecycle.viewModelScope
 import com.umbrella.weatherappmvvp.models.City
 import com.umbrella.weatherappmvvp.network.RetroInstance
 import com.umbrella.weatherappmvvp.network.RetroService
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivityViewModel : ViewModel() {
 
@@ -15,17 +14,25 @@ class MainActivityViewModel : ViewModel() {
 
     fun makeApiCall() {
         viewModelScope.launch(Dispatchers.IO) {
-            val retroInstance = RetroInstance.getRetroInstance().create(RetroService::class.java)
-            val response1 = retroInstance.getDataFromApi("55.75", "37.61")
-            response1.name = "Москва"
-            val response2 = retroInstance.getDataFromApi("59.93", "30.33")
-            response2.name = "Санкт-Петербург"
-            val response3 = retroInstance.getDataFromApi("62.03", "129.67")
-            response3.name = "Якутск"
             val cities = mutableListOf<City>()
-            cities.add(response1)
-            cities.add(response2)
-            cities.add(response3)
+            val retroInstance = RetroInstance.getRetroInstance().create(RetroService::class.java)
+            joinAll(
+                launch {
+                    val response1 = retroInstance.getDataFromApi("55.75", "37.61")
+                    response1.name = "Москва"
+                    cities.add(response1)
+                },
+                launch {
+                    val response2 = retroInstance.getDataFromApi("59.93", "30.33")
+                    response2.name = "Санкт-Петербург"
+                    cities.add(response2)
+                },
+                launch {
+                    val response3 = retroInstance.getDataFromApi("62.03", "129.67")
+                    response3.name = "Якутск"
+                    cities.add(response3)
+                }
+            )
             citiesLiveData.postValue(cities)
         }
     }
